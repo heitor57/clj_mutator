5c69619dffdb3a1d9dc8e5563008c1027db4ee67
Heitor <heitorwerneck@hotmail.com>
2018-09-05 23:23:08 -0300

    indent

--- mutant-tool/src/mutant_tool/mutator.clj
+++ mutant-tool/src/mutant_tool/mutator.clj
@@ -1,17 +1,20 @@
 (ns mutant-tool.mutator)
 (defn createop
-   "Used to generate a hashmap of operators and their relationship
-   (createop [\"or\" \"and\" \"+\" \"-\"])
-   that generate: {:or 'and, :and 'or, :+ '-, :- '+}"
+  "Used to generate a hashmap of operators and their relationship
+  (createop [\"or\" \"and\" \"+\" \"-\"])
+  that generate: {:or 'and, :and 'or, :+ '-, :- '+}"
   [value]
   (loop [queue value
-                       finalval {}]
-                  (if (empty? queue)
-                    finalval
-                    (let [[part sec & remaining] queue]
-                      (recur 
-                        remaining 
-                        (into finalval {(keyword part) (symbol sec) (keyword sec) (symbol part)}))))))
-    
+         finalval {}]
+    (if (empty? queue)
+      finalval
+      (let [[part sec & remaining] queue]
+        (recur 
+          remaining 
+          (into finalval {(keyword part) (symbol sec) (keyword sec) (symbol part)}))))))
+
 
 (def operators (createop ["or" "and" "empty?" "seq"]))
+
+
+
