be4c29492a52bbaf04a035dcbdbb81f4aef92e08
Heitor <heitorwerneck@hotmail.com>
2018-09-05 21:23:13 -0300

    value type string to symbol and macro to function

--- mutant-tool/src/mutant_tool/mutator.clj
+++ mutant-tool/src/mutant_tool/mutator.clj
@@ -1,17 +1,17 @@
 (ns mutant-tool.mutator)
-(defmacro createop
+(defn createop
    "Used to generate a hashmap of operators and their relationship
-   (createop varname [\"or\" \"and\" \"+\" \"-\"])
-   that generate: {:or \"and\", :and \"or\", :+ \"-\", :- \"+\"}"
-  [aka value]
-  (let [finalval (loop [queue value
+   (createop [\"or\" \"and\" \"+\" \"-\"])
+   that generate: {:or 'and, :and 'or, :+ '-, :- '+}"
+  [value]
+  (loop [queue value
                        finalval {}]
                   (if (empty? queue)
                     finalval
                     (let [[part sec & remaining] queue]
                       (recur 
                         remaining 
-                        (into finalval {(keyword part) (identity sec) (keyword sec) (identity part)})))))]
-    `(def ~aka ~finalval)))
+                        (into finalval {(keyword part) (symbol sec) (keyword sec) (symbol part)}))))))
+    
 
-(createop operators ["or" "and" "+" "-" "empty?" "seq"])
+(def operators (createop ["or" "and" "empty?" "seq"]))
