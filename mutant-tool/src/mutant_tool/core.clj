2a9a1215e5fc57fca405481b02b327bb9cc97fab
Heitor <heitorwerneck@hotmail.com>
2018-09-26 23:02:52 -0300

    core.clj test template

--- mutant-tool/src/mutant_tool/core.clj
+++ mutant-tool/src/mutant_tool/core.clj
@@ -2,8 +2,14 @@
   (:require [mutant-tool.mutator :as mut])
   (:require [mutant-tool.filehelper :as fh])
   (:gen-class))
-
+(def code-test "(+ 1 2)
+(+ 3 2)
+(or nil 1)")
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (fh/mapoperators "(+ 2 3 (+ 5 2))")))
+  (println (fh/mapoperators "(+ 2 3 (+ 5 2))"))
+  (println (fh/expandstr "(when (> 2 1) (println \"Nice\"))"))
+  (println (fh/mapoperators (fh/expandstr code-test)))
+  (println (eval (read-string (fh/expandstr code-test))))
+  )
