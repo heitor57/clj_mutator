34c72fbfba6bd338a1c22f2661ef4208faae0286
Heitor <heitorwerneck@hotmail.com>
2018-10-09 18:59:39 -0300

    modularization

--- mutant-tool/src/mutant_tool/filehelper.clj
+++ mutant-tool/src/mutant_tool/filehelper.clj
@@ -39,13 +39,17 @@
        )
   )
 
+(defn- read-string-all
+  [text]
+  (read-string (str "[" text "]"))
+  )
 (defn expandstr
   "This function was made to be used with mapoperators to expand all the text and catch all operators that may be hidden
   Example:
   Entry: \"(when (> 2 1) (println \"Nice\"))\"
   Result: \"[(if (> 2 1) (do (println \"Nice\")))]\""
   [text]
-  (str (walk/macroexpand-all (read-string (str "[" text "]"))))
+  (str (walk/macroexpand-all (read-string-all text)))
   )
 
 (defn mapoperators
