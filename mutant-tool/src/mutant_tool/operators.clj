1e70ea7324fcf27ebad902b81dc429ea4730012c
Heitor <heitorwerneck@hotmail.com>
2018-10-10 20:54:35 -0300

    creation of multi related operators

--- mutant-tool/src/mutant_tool/operators.clj
+++ mutant-tool/src/mutant_tool/operators.clj
@@ -1,19 +1,25 @@
 (ns mutant-tool.operators)
-(def opstr ["+" "-" "or" "and" "empty?" "seq"])
+(def opstr [["+" "-" "*" "/"] ["or" "and"] ["empty?" "seq"]])
 (defn ^:private createop
   "Used to generate a hashmap of operators and their relationship
-  (createop [\"or\" \"and\" \"+\" \"-\"])
-  that generate: {:or 'and, :and 'or, :+ '-, :- '+}"
+  (createop [[\"+\" \"-\" \"*\" \"/\"] [\"or\" \"and\"] [\"empty?\" \"seq\"]])
+  that generate: {:+ (\"-\" \"*\" \"/\"), :- (\"+\" \"*\" \"/\"), :* (\"+\" \"-\" \"/\"), :/ (\"+\" \"-\" \"*\"), :or (\"and\"), :and (\"or\"), :empty? (\"seq\"), :seq (\"empty?\")}"
   [value]
   (loop [queue value
-         finalval {}]
+         finalval {}] 
     (if (empty? queue)
       finalval
-      (let [[part sec & remaining] queue]
-        (recur 
-          remaining 
-          (into finalval {(keyword part) (symbol sec) (keyword sec) (symbol part)}))))))
-
+      (recur 
+        (rest queue) 
+        (into 
+          finalval 
+          (into [] 
+                (for [op (first queue)] 
+                  (conj [] (keyword op) (filter #(not= op %) (first queue))))))
+        )
+      )
+    )
+  )
 
 (def operators (createop opstr))
 
