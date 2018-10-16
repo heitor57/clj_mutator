58a03ba71b3f0ca9cbe3be012ddfe162c7c412d0
Heitor <heitorwerneck@hotmail.com>
2018-10-16 18:23:11 -0300

    Update project.clj

--- mutant-tool/project.clj
+++ mutant-tool/project.clj
@@ -1,8 +1,8 @@
 (defproject mutant-tool "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [rewrite-clj "0.6.1"]
                  [clj-diffmatchpatch "0.0.9.3"]
